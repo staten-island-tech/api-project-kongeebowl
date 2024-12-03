e8082176d7f2b4759ef64a8664ff6d9c3005feb0
kongeebowl <kadenp11@nycstudents.net>
2024-12-03 11:55:00 -0500

    n

--- app/js/main.js
+++ app/js/main.js
@@ -30,7 +30,7 @@ function addCards(data) {
       DOMSelectors.box.insertAdjacentHTML(
         "beforeend",
         `
-      <div class="card w-1/5 sm:w-3/5 md:w-2/5 lg:w-1/5 h-auto  bg-[#150709] rounded-3xl flex flex-col items-center justify-around m-8 border-2 border-[#e0d1ad] font-hylia">
+      <div class="card w-[90%] sm:w-4/5 md:w-2/5 lg:w-1/5 h-auto  bg-[#150709] rounded-3xl flex flex-col items-center justify-around m-8 border-2 border-[#e0d1ad] font-hylia">
             <h2 class="card-title text-2xl text-[#e0d1ad]">${obj.name}</h2>
             <img src="${obj.image}" alt="${obj.name}, ${obj.category}, ${obj.common_locations}" class="object-contain w-[65%]" />
             <p class="text-base text-[#87a4b4]">${obj.id}</p>
